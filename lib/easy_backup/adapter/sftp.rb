095e1f29f5a2beedab529f01691919a4fa19a99e
gabriel <gnaiman@keepcon.com>
2012-06-22 15:51:46 -0300

    SFTP log process

--- lib/easy_backup/adapter/sftp.rb
+++ lib/easy_backup/adapter/sftp.rb
@@ -25,11 +25,34 @@ module EasyBackup
 
       def save(resource)
         Net::SFTP.start(host, username, :password => password) do |sftp|
-          EasyBackup.logger.info "[SFTP] Saving #{resource}\n#{' '*9}into #{host} | #{username} | #{folder}"
+          sftp_folder = folder
+          EasyBackup.logger.info "[SFTP] Saving #{resource}\n#{' '*9}into #{host} | #{username} | #{sftp_folder}"
 
-          sftp.mkpath! folder
-          sftp.upload! resource, "#{folder}/#{File.basename(resource)}"
+          sftp.mkpath! sftp_folder, :progress => SFTPHandler.new
+          sftp.upload! resource, "#{sftp_folder}/#{File.basename(resource)}", :progress => SFTPHandler.new
         end
+      end
+    end
+
+    class SFTPHandler
+      def on_open(uploader, file)
+        EasyBackup.logger.info "#{' '*9}starting upload: #{file.local} -> #{file.remote} (#{file.size} bytes)"
+      end
+
+      def on_put(uploader, file, offset, data)
+        EasyBackup.logger.info "#{' '*9}#{data.length} bytes to #{file.remote} starting at #{offset}"
+      end
+
+      def on_close(uploader, file)
+        EasyBackup.logger.info "#{' '*9}finished with #{file.remote}"
+      end
+
+      def on_mkdir(uploader, path)
+        EasyBackup.logger.info "#{' '*9}creating directory #{path}"
+      end
+
+      def on_finish(uploader)
+        EasyBackup.logger.info "#{' '*9}all done!"
       end
     end
   end
--- lib/easy_backup/extension/net_sftp_session.rb
+++ lib/easy_backup/extension/net_sftp_session.rb
@@ -1,10 +1,10 @@
 module Net
   module SFTP
     class Session
-      def mkpath!(path)
+      def mkpath!(path, options={})
         path.split('/').inject(nil) do |previous, relative|
           current = previous ? "#{previous}/#{relative}" : relative
-          mkdir! current if dir.glob('.', current).empty?
+          mkdir! current, options if dir.glob('.', current).empty?
           current
         end
       end
